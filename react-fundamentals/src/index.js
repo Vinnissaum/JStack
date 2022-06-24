b27e434f19153173c07ca2a80f313a3ccccc233d
Vinicius L S Braz <vinicius_lsb@live.com>
2022-06-23 22:03:47 -0300

    first webpack settings

--- react-fundamentals/src/index.js
+++ react-fundamentals/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react';
+import ReactDOM from 'react-dom';
 
 ReactDOM.render(
   <h1 id='title'>
-    <span>Hello World!</span>
+    <span>Hello World!!!</span>
   </h1>,
   document.getElementById('root'),
 );
