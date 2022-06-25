c3e3e33a1a7518dbaeace32a17bf67f7a022ee92
Vinicius L S Braz <vinicius_lsb@live.com>
2022-06-25 18:53:07 -0300

    callback functions props

--- react-fundamentals/src/App.js
+++ react-fundamentals/src/App.js
@@ -7,22 +7,26 @@ const subtitle = 'Post subtitle';
 
 function App() {
   const [posts, setPosts] = useState([
-    { title: 'Title#01', subtitle: 'Sub#01', likes: 10 },
-    { title: 'Title#02', subtitle: 'Sub#02', likes: 30 },
-    { title: 'Title#03', subtitle: 'Sub#03', likes: 50},
+    { id: Math.random(), title: 'Title#01', subtitle: 'Sub#01', likes: 10 },
+    { id: Math.random(), title: 'Title#02', subtitle: 'Sub#02', likes: 30 },
+    { id: Math.random(), title: 'Title#03', subtitle: 'Sub#03', likes: 50},
   ]);
 
   function handleRefresh() {
-    setTimeout(() => {
-      setPosts((prevState) => [
-        ...prevState,
-        {
-          title: `Title#0${prevState.length +1}`, 
-          subtitle: `Sub#0${prevState.length +1}`, 
-          likes: 10
-        }
-      ]);
-    }, 2000);
+    setPosts((prevState) => [
+      ...prevState,
+      {
+        title: `Title#0${prevState.length +1}`, 
+        subtitle: `Sub#0${prevState.length +1}`, 
+        likes: 10
+      }
+    ]);
+  }
+
+  function handleRemove(postId) {
+    setPosts(prevState => (
+      prevState.filter(post => post.id !== postId)
+    ));
   }
 
   return (
@@ -39,8 +43,10 @@ function App() {
         <Post 
           key={index}
           likes={post.likes}
+          onRemove={handleRemove}
           post={
-            {
+            { 
+              id: post.id,
               title: post.title,
               subtitle: post.subtitle
             }
