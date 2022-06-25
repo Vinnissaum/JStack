5d78c3673d79342d7c191a5b2c55d379ffc713fb
Vinicius L S Braz <vinicius_lsb@live.com>
2022-06-25 18:00:28 -0300

    useState introduction

--- react-fundamentals/src/App.js
+++ react-fundamentals/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Post from './Post';
 import Header from './Header';
 
@@ -6,40 +6,47 @@ const title = 'Post title';
 const subtitle = 'Post subtitle';
 
 function App() {
+  const [posts, setPosts] = useState([
+    { title: 'Title#01', subtitle: 'Sub#01', likes: 10 },
+    { title: 'Title#02', subtitle: 'Sub#02', likes: 30 },
+    { title: 'Title#03', subtitle: 'Sub#03', likes: 50},
+  ]);
+
+  function handleRefresh() {
+    setTimeout(() => {
+      setPosts((prevState) => [
+        ...prevState,
+        {
+          title: `Title#0${prevState.length +1}`, 
+          subtitle: `Sub#0${prevState.length +1}`, 
+          likes: 10
+        }
+      ]);
+    }, 2000);
+  }
+
   return (
     <>
       <Header>
-        <h2>Post of the week</h2>
+        <h2>
+          Post of the week 
+          <button onClick={handleRefresh}>Refresh</button>
+        </h2>
       </Header>
       <hr />
       
-      <Post 
-        likes={20}
-        post={
-          {
-            title,
-            subtitle,
+      {posts.map((post, index) => (
+        <Post 
+          key={index}
+          likes={post.likes}
+          post={
+            {
+              title: post.title,
+              subtitle: post.subtitle
+            }
           }
-        }
-      />
-      <Post 
-        likes={10}
-        post={
-          {
-            title,
-            subtitle,
-          }
-        }
-      />
-      <Post 
-        likes={50}
-        post={
-          {
-            title,
-            subtitle,
-          }
-        }
-      />
+        />
+      ))}
     </>
   );
 }
--- react-fundamentals/src/Header.js
+++ react-fundamentals/src/Header.js
@@ -2,11 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export default function Header(props) {
-  return <h1>{props.title}</h1>;
+  return(
+    <>
+      <h1>{props.title}</h1>
+      {props.children}
+    </>
+  ); 
 }
 
 Header.propTypes = {
   title: PropTypes.string,
+  children: PropTypes.node
 }
 
 Header.defaultProps = {
