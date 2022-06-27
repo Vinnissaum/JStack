007ee6cea9a08698ec28a577dd1e1a4a41c15368
Vinicius L S Braz <vinicius_lsb@live.com>
2022-06-27 10:31:12 -0300

    style: feature css modules added

--- react-fundamentals/src/Header.js
+++ react-fundamentals/src/Header.js
@@ -3,12 +3,14 @@ import PropTypes from 'prop-types';
 import { themeContext } from './ThemeContext';
 import Button from './Button';
 
+import styles from './Header.css';
+
 export default function Header(props) {
   const { onToggleTheme } = useContext(themeContext);
 
   return (
     <>
-      <h1>{props.title}</h1>
+      <h1 className={styles.title}>{props.title}</h1>
       {props.children}
       <Button 
         onClick={onToggleTheme}
