64073b3ace19cb26c45ab3e48e8a2e8bfea51b9f
Vinicius Braz <vinicius_lsb@live.com>
2022-06-27 20:50:50 -0300

    multiple styled in the same function(props)

--- react-fundamentals/src/components/Post/styles.js
+++ react-fundamentals/src/components/Post/styles.js
@@ -1,9 +1,14 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 export const Container = styled.article`
   margin-bottom: 30px;
-  opacity: ${props => props.removed ? 0.5 : 1};
-  color: ${props => props.removed ? '#F00' : '#FFF'};
+  /* opacity: ${props => props.removed ? 0.5 : 1};
+  color: ${props => props.removed ? '#F00' : '#FFF'}; */
+
+  ${({ removed }) => css`
+    opacity: ${removed ? 0.5 : 1};
+    color: ${removed ? '#F00' : '#FFF'};
+  `}
 `;
 
 export const Subtitle = styled.small`
