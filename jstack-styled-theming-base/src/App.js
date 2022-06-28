be60fba93b9410297985397b2d5c7e87c0acefbc
Vinicius Braz <vinicius_lsb@live.com>
2022-06-28 11:49:57 -0300

    Setting ThemeProvider first features

--- jstack-styled-theming-base/src/App.js
+++ jstack-styled-theming-base/src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
-
+import { ThemeProvider } from 'styled-components';
 import GlobalStyle from './styles/global';
 import Layout from './components/Layout';
+import themes from './styles/themes';
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={themes.dark}>
       <GlobalStyle />
       <Layout />
-    </>
+    </ThemeProvider>
   );
 };
 
