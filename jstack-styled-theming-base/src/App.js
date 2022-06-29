dca8dc061efb5878bb8e44e15d353b3736c342eb
Vinicius Braz <vinicius_lsb@live.com>
2022-06-29 10:15:31 -0300

    refactor: themeContext handles every theme features singly

--- jstack-styled-theming-base/src/App.js
+++ jstack-styled-theming-base/src/App.js
@@ -1,29 +1,17 @@
-import React, { useState, useMemo } from 'react';
-import { ThemeProvider } from 'styled-components';
-import { ThemeController } from './context/themeContext';
+import React from 'react';
 import useLocalState from './hooks/useLocalState';
+
 import GlobalStyle from './styles/global';
 import Layout from './components/Layout';
+import { ThemeContextProvider } from './context/themeContext';
 import themes from './styles/themes';
 
 function App() {
-  const [theme, setTheme] = useLocalState('themeKey', 'light');
-
-  const currentTheme = useMemo(() => (themes[theme] || themes.dark), [theme]);
-
-  function handleToggleTheme() {
-    setTheme(prevState => prevState === 'dark' ? 'light' : 'dark');
-  }
   return (
-    <ThemeProvider theme={currentTheme}>
-      <ThemeController 
-        onToggleTheme={handleToggleTheme}
-        selectedTheme={theme}  
-      >
+    <ThemeContextProvider>
         <GlobalStyle />
         <Layout />
-      </ThemeController>
-    </ThemeProvider>
+    </ThemeContextProvider>
   );
 };
 
--- jstack-styled-theming-base/src/context/themeContext.js
+++ jstack-styled-theming-base/src/context/themeContext.js
@@ -1,18 +1,32 @@
-import React, { createContext } from 'react';
+import React, { useMemo, createContext } from 'react';
+import { ThemeProvider } from 'styled-components';
+
+import themes from '../styles/themes';
+import useLocalState from '../hooks/useLocalState';
 
 export const themeContext = createContext();
 
-export function ThemeController( props ){
+export function ThemeContextProvider({ children }) {
+  const [theme, setTheme] = useLocalState('theme', 'light');
+
+  const currentTheme = useMemo(() => (themes[theme] || themes.dark), [theme]);
+
+  function handleToggleTheme() {
+    setTheme(prevState => prevState === 'dark' ? 'light' : 'dark');
+  }
+  
   return (
     <themeContext.Provider
-      value={
-        {
-          onToggleTheme: props.onToggleTheme,
-          selectedTheme: props.selectedTheme
-        }
+     value={
+      { 
+        onToggleTheme: handleToggleTheme,
+        selectedTheme: theme,
       }
+     }   
     >
-      {props.children}
+      <ThemeProvider theme={currentTheme}>
+        {children}
+      </ThemeProvider>
     </themeContext.Provider>
   );
 }
