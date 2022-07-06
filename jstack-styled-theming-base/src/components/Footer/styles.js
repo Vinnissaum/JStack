5a0c7b29b21d58f0569758f4e718cd1cf5f0efcf
Vinicius Braz <vinicius_lsb@live.com>
2022-07-06 11:55:03 -0300

    feat: animated routes "react-spring"

--- jstack-styled-theming-base/src/components/Footer/styles.js
+++ jstack-styled-theming-base/src/components/Footer/styles.js
@@ -1,14 +1,18 @@
 import styled from 'styled-components';
 
 export const Container = styled.footer`
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: 0;
   background: ${({ theme }) => theme.footerBackgroundColor };
   height: 70px;
   display: flex;
   align-items: center;
   padding: 0 24px;
+  margin: 10px;
   border-radius: 10px;
   justify-content: space-between;
-  margin-top: 24px;
 
   button {
     cursor: pointer;
