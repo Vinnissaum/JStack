ea1e7548b17c42a068be5697f25ce49bb912d4b3
Vinicius Braz <vinicius_lsb@live.com>
2022-07-05 23:11:57 -0300

    feat: linking the routes

--- jstack-styled-theming-base/src/components/Layout/index.js
+++ jstack-styled-theming-base/src/components/Layout/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Link } from 'react-router-dom';
 
 import Header from '../Header';
 import Footer from '../Footer';
 import Route from '../../Routes';
+import { Nav } from './styles';
 
 export default function Layout() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
+      <nav>
+        <Link to='/'>Home</Link>
+        <Link to='/posts'>Posts</Link>
+      </nav>
       <Route />
       <Footer /> 
-    </>
+    </BrowserRouter>
   );
 }
