37170b64b54eea417b36858f55f654e700c1a397
Vinicius Braz <vinicius_lsb@live.com>
2022-07-06 09:41:59 -0300

    refactor: class component with URL params

--- jstack-styled-theming-base/src/pages/Post/index.js
+++ jstack-styled-theming-base/src/pages/Post/index.js
@@ -1,12 +1,26 @@
 import React, { useMemo } from "react";
 import { useParams, useLocation } from "react-router-dom";
 
-export default function Post() {
-  const params = useParams();
-  const { search } = useLocation();
-  const queryParams = useMemo(() => new URLSearchParams(search), [search]);
+export default class Post extends React.Component {
+  constructor(props) {
+    super(props);
+    
+    const { search } = this.props.location;
+    this.queryParams = new URLSearchParams(search);
+  }
 
-  console.log(queryParams.get('query'));
-
-  return <h1>Post Page</h1>;
+  render() {
+    console.log(this.queryParams.get('queryParams'));
+    return <h1>Post Page</h1>;
+  }
 }
+
+// export default function Post() {
+//   const params = useParams();
+//   const { search } = useLocation();
+//   const queryParams = useMemo(() => new URLSearchParams(search), [search]);
+
+//   console.log(queryParams.get('query'));
+
+//   return <h1>Post Page</h1>;
+// }
